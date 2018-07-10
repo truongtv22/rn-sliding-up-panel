4f4f56fd5f31f7b62905995fdf66c9a2c97df6a2
Tạ Văn Trường <truongtv22@gmail.com>
2018-07-10 09:50:51 +0700

    Update styles.js

--- libs/styles.js
+++ libs/styles.js
@@ -1,6 +1,13 @@
 import {StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
   backdrop: {
     position: 'absolute',
     top: 0,
